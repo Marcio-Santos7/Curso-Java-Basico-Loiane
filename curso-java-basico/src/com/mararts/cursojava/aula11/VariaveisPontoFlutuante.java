ca047fee02bf99e59fde6eeb8a3dcd2a82947457
Marcio Leandro <marcio449@hotmail.com>
2019-07-14 10:24:59 -0300

    Alteração do autor e inclusão de comentário de descrição da classe VariaveisPontoFlutuante

--- curso-java-basico/src/com/mararts/cursojava/aula11/VariaveisPontoFlutuante.java
+++ curso-java-basico/src/com/mararts/cursojava/aula11/VariaveisPontoFlutuante.java
@@ -6,8 +6,8 @@
 package com.mararts.cursojava.aula11;
 
 /**
- *
- * @author SONY VAIO
+ * Utilizando variáveis de ponto flutuante.
+ * @author Marcio Leandro
  */
 public class VariaveisPontoFlutuante {
     
